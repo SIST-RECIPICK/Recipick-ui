886dde73b484bd40e050a68fe1c76ea1c8ec0c5a
LeeSomgyul <rmawl8600@naver.com>
2026-09-14 02:52:16 +0900

    [이솜귤] 토큰 재발급 api 연결

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,4 +1,7 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const API_BASE = 'http://localhost:8080'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
@@ -6,7 +9,7 @@ export const useAuthStore = defineStore('auth', {
     accessToken: null,
   }),
   getters: {
-    isLoggedIn: (state) => !!state.accessToken,
+    isLoggedIn: (state) => !!state.accessToken && !!state.user,
     initial: (state) => state.user?.nickname?.[0] ?? '',
     isAdmin: (state) => state.user?.role === 'ADMIN',
   },
@@ -14,6 +17,20 @@ export const useAuthStore = defineStore('auth', {
     setUser({ userId, nickname, role, accessToken }) {
       this.user = { userId, nickname, role }
       this.accessToken = accessToken
+    },
+    setAccessToken(accessToken) {
+      this.accessToken = accessToken
+    },
+    setUserInfo({ userId, nickname, role }) {
+      this.user = { userId, nickname, role }
+    },
+    async fetchMe() {
+      try {
+        const res = await axios.get(`${API_BASE}/auth/me`, { withCredentials: true })
+        this.setUserInfo(res.data)
+      } catch {
+        this.logout()
+      }
     },
     logout() {
       // TODO: 서버 로그아웃(POST /auth/logout) 연동은 별도 작업으로 진행 예정
