f61679af6d92e9e60633f4bc54568676398c0395
LeeSomgyul <rmawl8600@naver.com>
2026-09-14 02:19:25 +0900

    [이솜귤] 로그인 api 연결

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,10 +1,24 @@
 import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore('auth',{
-  state:()=>({
-    user:null
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    accessToken: null,
   }),
-  actions:{
-    
-  }
-});
+  getters: {
+    isLoggedIn: (state) => !!state.accessToken,
+    initial: (state) => state.user?.nickname?.[0] ?? '',
+    isAdmin: (state) => state.user?.role === 'ADMIN',
+  },
+  actions: {
+    setUser({ userId, nickname, role, accessToken }) {
+      this.user = { userId, nickname, role }
+      this.accessToken = accessToken
+    },
+    logout() {
+      // TODO: 서버 로그아웃(POST /auth/logout) 연동은 별도 작업으로 진행 예정
+      this.user = null
+      this.accessToken = null
+    },
+  },
+})
