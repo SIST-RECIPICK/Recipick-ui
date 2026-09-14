636322eab4285f3fc137f49c8b57441036b15e6c
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-14 21:15:27 +0900

    fhzmx2: make test layout

--- src/views/mypage/MyPageMyReply.vue
+++ src/views/mypage/MyPageMyReply.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>저장한 레시피</h1>
+    <h4>< 마이페이지 메인으로</h4>
+    <h1>나의 댓글 (164)</h1>
+    
+    <article class="card">
+        <div class="card__body">
+
+            <h6>원본 글</h6>
+            <p>홍길동 · 2026-09-14</p>
+        </div>
+    </article>
+
+    <article class="card">
+        <div class="card__body">
+
+            <h6>원본 글</h6>
+            <p>홍길동 · 2026-09-14</p>
+        </div>
+    </article>
+
+    <article class="card">
+        <div class="card__body">
+
+            <h6>원본 글</h6>
+            <p>홍길동 · 2026-09-14</p>
+        </div>
+    </article>
+    
+    <div class="container">
+        <Pagination></Pagination>
+    </div>
 </template>
+
+<script setup lang="ts">
+import Pagination from '@/components/common/Pagination.vue';
+</script>
--- src/views/mypage/MyPageMyReview.vue
+++ src/views/mypage/MyPageMyReview.vue
@@ -1,16 +1,36 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
     <h4>< 마이페이지 메인으로</h4>
     <h1>나의 리뷰 (27)</h1>
     
     <article class="card">
         <div class="card__body">
+
             <h3>제목입니다</h3>
             <p>홍길동 · 2026-09-14</p>
         </div>
     </article>
 
+    <article class="card">
+        <div class="card__body">
+
+            <h3>제목입니다</h3>
+            <p>홍길동 · 2026-09-14</p>
+        </div>
+    </article>
+
+    <article class="card">
+        <div class="card__body">
+
+            <h3>제목입니다</h3>
+            <p>홍길동 · 2026-09-14</p>
+        </div>
+    </article>
+    
+    <div class="container">
+        <Pagination></Pagination>
+    </div>
 </template>
+
+<script setup lang="ts">
+import Pagination from '@/components/common/Pagination.vue';
+</script>
